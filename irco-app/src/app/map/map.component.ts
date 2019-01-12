49fc9b9e72a71e83b388c57158e3be35c98bd8bc
Lauren Namdar <lenamdar@ucdavis.edu>
2019-01-12 13:12:49 -0800

    Don't drop a pin every time someone clicks the map

--- irco-app/src/app/map/map.component.ts
+++ irco-app/src/app/map/map.component.ts
@@ -17,14 +17,6 @@ export class MapComponent {
     console.log(`clicked the marker: ${label || index}`)
   }
 
-  mapClicked($event: MouseEvent) {
-    this.markers.push({
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: true
-    });
-  }
-
   markerDragEnd(m: marker, $event: MouseEvent) {
     console.log('dragEnd', m, $event);
   }
